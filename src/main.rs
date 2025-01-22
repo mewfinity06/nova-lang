594217c8aeb4d3ffaf4a9755ac792d3c4c64294e
Ashton Sickler <ashton.sickler@icloud.com>
2025-01-21 20:13:55 -0600

    [src/main] Added Command struct, added clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let cmd = Command::parse();
+
+    println!("{:?}", cmd);
+}
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = "Welcome to the Nova Language Compiler")]
+struct Command {
+    /// The input file to use
+    file_path: String,
+
+    /// The output file to use
+    #[arg(short, default_value = "main")]
+    output: Option<String>,
+
+    /// Enable debug mode | To be deprecated
+    #[arg(short, long)]
+    debug: bool,
+
+    /// Enable developer mode | To be deprecated
+    #[arg(long)]
+    dev: bool,
 }
