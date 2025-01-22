e80bb0f285b0725f1dc8f2bb79c387190c72e906
Ashton Sickler <ashton.sickler@icloud.com>
2025-01-21 21:00:25 -0600

    [src/main] Added get_contents.. with mut buffer

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
+
 use clap::Parser;
+use lexer::Lexer;
+
+mod lexer;
+
+fn get_contents_via_mut_buffer(file_path: &String) -> String {
+    let mut data = String::new();
+    let f = File::open(file_path).expect("Unable to open file");
+    let mut br = BufReader::new(f);
+    br.read_to_string(&mut data).expect("Unable to read string");
+    data
+}
 
 fn main() {
     let cmd = Command::parse();
 
-    println!("{:?}", cmd);
-}
+    let mut lexer = Lexer::new(cmd.file_path.to_string(), get_contents_via_mut_buffer(&cmd.file_path));
+    let tokens = lexer.lex();
 
+    for token in tokens {
+        println!("{}", token);
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = "Welcome to the Nova Language Compiler")]
