1afe02077c4a8d31b59bd36fc4f3f73699b543d8
Ashton Sickler <ashton.sickler@icloud.com>
2025-01-27 01:18:38 -0600

    Finished ndl branch. to be merged

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,3 +1,5 @@
+use std::{path::Path, process::Command};
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -6,4 +8,70 @@ pub struct Config {
     vec_growth_factor: f64,
     dev_mode: bool,
     debug: bool,
+    output_file: String
+}
+
+pub fn get_config() -> anyhow::Result<Config> {
+    ///////////// Build NDL Parser ///////////////////////
+
+    // May not work on other operating systems from Linux
+    let dir_path = "ndl-parser"; // The directory that ndl-parser should live in
+    let exe_name = "ndl-parser"; // Linux executables usually don’t have ".exe"
+
+    let exe_path = Path::new(dir_path).join(exe_name);
+
+    if !directory_exists(dir_path) {
+        get_ndl_parser_from_github()?;
+    } else {
+        update_ndl_parser()?;
+    }
+
+    // If ndl-parser does not exits
+    if !exe_path.exists() && !exe_path.is_file() {
+        build_ndl_parser()?;
+    }
+
+    ///////////// Use NDL Parser ///////////////////////
+
+    let ndl_parser = Command::new(exe_path).arg(".").output()?;
+
+    let ndl_parsed = format!("{}", String::from_utf8_lossy(&ndl_parser.stdout));
+
+    let def: Config = serde_json::from_str(&ndl_parsed)?;
+    Ok(def)
+}
+
+fn get_ndl_parser_from_github() -> anyhow::Result<()> {
+    println!("Getting ndl-parser from Github");
+
+    Command::new("sh")
+        .arg("-c")
+        .arg("git clone https://github.com/mewfinity06/ndl-parser.git")
+        .output()?;
+
+    Ok(())
+}
+
+fn build_ndl_parser() -> anyhow::Result<()> {
+    println!("Building ndl-parser");
+
+    Command::new("sh")
+        .arg("-c")
+        .arg("cd ndl-parser && go build -o ndl-parser main.go && cd ..")
+        .output()?;
+
+    Ok(())
+}
+
+fn directory_exists(dir_path: &str) -> bool {
+    Path::new(dir_path).is_dir()
+}
+
+fn update_ndl_parser() -> anyhow::Result<()> {
+    Command::new("sh")
+        .arg("-c")
+        .arg("cd ndl-parser && git pull")
+        .output()?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,28 @@
 use std::fs::File;
 use std::io::{BufReader, Read};
-use std::path::Path;
-use std::process::Command as RustCommand;
 
-mod lexer;
 mod config;
+mod lexer;
 use clap::Parser;
 
 use lexer::Lexer;
-use config::Config;
 
 fn main() -> anyhow::Result<()> {
-    ///////////// Build NDL Parser ///////////////////////
+    let cli = Cli::parse();
+    let _cfg = config::get_config().expect("Was not able to get config");
 
-    // May not work on other operating systems from Linux
-    let dir_path = "ndl-parser"; // The directory that ndl-parser should live in
-    let exe_name = "ndl-parser"; // Linux executables usually don’t have ".exe"
+    let mut lexer = Lexer::new(
+        cli.file_path.to_string(),
+        get_contents_via_mut_buffer(&cli.file_path),
+    );
 
-    let exe_path = Path::new(dir_path).join(exe_name);
+    let tokens = lexer.lex();
 
-    if !directory_exists(dir_path) {
-        get_ndl_parser_from_github()?;
+    for token in &tokens {
+        println!("{:?}", token);
     }
 
-    // If ndl-parser does not exits
-    if !exe_path.exists() && !exe_path.is_file() {
-        build_ndl_parser()?;
-    }
-
-    ///////////// Use NDL Parser ///////////////////////
-    
-    let ndl_parser = RustCommand::new(exe_path)
-        .arg(".")
-        .output()?;
-
-    let ndl_parsed = format!("{}", String::from_utf8_lossy(&ndl_parser.stdout));
-
-    println!("Parsed: {ndl_parsed}");
-
-    let def: Config = serde_json::from_str(&ndl_parsed)?;
-
-    println!("{:?}", def);
-
     Ok(())
-}
-
-fn get_ndl_parser_from_github() -> anyhow::Result<()> {
-    RustCommand::new("sh")
-        .arg("-c")
-        .arg("git clone https://github.com/mewfinity06/ndl-parser.git")
-        .output()?;
-
-    println!("Built ndl-parser dir");
-
-    Ok(())
-}
-
-fn build_ndl_parser() -> anyhow::Result<()> {
-    RustCommand::new("sh")
-        .arg("-c")
-        .arg("cd ndl-parser && go build -o ndl-parser main.go && cd ..")
-        .output()?;
-
-    println!("Build ndl-parser");
-
-    Ok(())
-}
-
-fn directory_exists(dir_path: &str) -> bool {
-    Path::new(dir_path).is_dir()
 }
 
 ///////////// IGNORE BELOW FOR THIS BRANCH ////////////////////
@@ -80,37 +34,13 @@ fn get_contents_via_mut_buffer(file_path: &String) -> String {
     data
 }
 
-fn main2() {
-    let cmd = Command::parse();
-
-    let mut lexer = Lexer::new(
-        cmd.file_path.to_string(),
-        get_contents_via_mut_buffer(&cmd.file_path),
-    );
-    let tokens = lexer.lex();
-
-    for token in &tokens {
-        println!("{}", token);
-    }
-
-    println!("Token len: {}", tokens.len());
-}
-
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = "Welcome to the Nova Language Compiler")]
-struct Command {
+struct Cli {
     /// The input file to use
     file_path: String,
 
     /// The output file to use
     #[arg(short, default_value = "main")]
     output: Option<String>,
-
-    /// Enable debug mode | To be deprecated
-    #[arg(short, long)]
-    debug: bool,
-
-    /// Enable developer mode | To be deprecated
-    #[arg(long)]
-    dev: bool,
 }
