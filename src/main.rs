391004bc351150d2ed326381396617541c80b921
Ashton Sickler <ashton.sickler@icloud.com>
2025-02-02 00:22:19 -0600

    Removed ndl-parser from Golang, added rustlib version of ndl-parser

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
 use std::fs::File;
 use std::io::{BufReader, Read};
 
-mod config;
 mod lexer;
 mod parser;
 
-use clap::Parser as ClapParser;
 use parser::Parser;
+
+use clap::Parser as ClapParser;
+use ndl_parser::*;
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
-    let _cfg = config::get_config().expect("Was not able to get config");
+    // let _cfg = config::get_config().expect("Was not able to get config");
+    let _cfg = get_config(&cli.config_path.unwrap_or_default()).unwrap_or_default();
 
     let mut p = Parser::new(
-        cli.file_path.to_string(),
+        cli.file_path.clone(),
         get_contents_via_mut_buffer(&cli.file_path),
     );
 
-    let _parsed_tokens = p.parse_lr1();
+    let parsed_tokens = p.parse_lr1();
+
+    println!("{:?}", parsed_tokens);
 
     Ok(())
 }
 
-fn get_contents_via_mut_buffer(file_path: &String) -> String {
+fn get_contents_via_mut_buffer(file_path: &str) -> String {
     let mut data = String::new();
     let f = File::open(file_path).expect("Unable to open file");
     let mut br = BufReader::new(f);
@@ -36,6 +40,10 @@ fn get_contents_via_mut_buffer(file_path: &String) -> String {
 struct Cli {
     /// The input file to use
     file_path: String,
+
+    /// Config file, if `Config.ndl` does not exist
+    #[arg(short, long)]
+    config_path: Option<String>,
 
     /// The output file to use
     #[arg(short, default_value = "main")]
